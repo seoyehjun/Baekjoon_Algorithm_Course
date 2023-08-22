699d691b59fd353ba4b04072a5296b7ca74a1f72
holy38 <63962696+seoyehjun@users.noreply.github.com>
2023-08-22 15:05:37 +0900

    10951

--- Baekjoon_Iteration/src/baek_10951/PlusAB.java
+++ Baekjoon_Iteration/src/baek_10951/PlusAB.java
@@ -1,4 +1,4 @@
-package baek_15552;
+package baek_10951;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -7,26 +7,23 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
-public class FastAB 
+public class PlusAB 
 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		StringTokenizer st;
+		String temp;
 		
-		int num = Integer.parseInt(br.readLine());
-		
-		for(int i=0;i<num;i++)
+		while((temp = br.readLine())!=null)
 		{
-			st = new StringTokenizer(br.readLine());
-			//st에 담는 이유는 " "기준으로 분리해서 받기 위해서?
-			bw.write((Integer.parseInt(st.nextToken()))+(Integer.parseInt(st.nextToken()))+"\n");
-			//bw.write의 매개변수에 +문자열을 해서 문자열 형식으로 바꾸어 주어야 한다.
-			//System.out.println("test");
+			st = new StringTokenizer(temp);
+			
+			bw.write((Integer.parseInt(st.nextToken())) + (Integer.parseInt(st.nextToken()))+"\n");
 		}
 		
 		bw.close();
-		
 	}
+	
 }
