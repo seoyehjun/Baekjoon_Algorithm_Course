8c54fb42be47bb8abff6daa154aea455b8cb7d56
서예준 <skgy322@gmail.com>
2023-08-24 22:08:18 +0900

    complete 3052

--- Baekjoon_oneDarray/src/baek_3052/Left.java
+++ Baekjoon_oneDarray/src/baek_3052/Left.java
@@ -7,12 +7,21 @@ public class Left
 	public static int exclusive(int[] list)
 	{
 		int count = 0;
+		int temp;
 		for(int i=0;i<list.length;i++)
 		{
-			
+			temp =0;
+			for(int j=i+1;j<list.length;j++)
+			{
+				//같은값이 하나라도 있으면 temp > 0 
+				//되어서 count되지 않는다
+				if(list[i]==list[j]) temp++;
+			}
+			if (temp==0)count ++;
 		}
-		return 0;
+		return count;
 	}
+	
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
@@ -24,11 +33,6 @@ public class Left
 			list[i] = sc.nextInt() % 42 ;
 		}
 		
-		
-		
-		
+		System.out.print(exclusive(list));
 	}
-	
-	
-	
 }
