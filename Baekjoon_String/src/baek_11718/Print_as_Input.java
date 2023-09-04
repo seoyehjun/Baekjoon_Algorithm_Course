ff50327fa773032100d88432b62c4aa6c7b32425
holy38 <63962696+seoyehjun@users.noreply.github.com>
2023-09-04 09:44:26 +0900

    String Finished

--- Baekjoon_String/src/baek_11718/Print_as_Input.java
+++ Baekjoon_String/src/baek_11718/Print_as_Input.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Print_as_Input 
+public class Print_as_Input
 {
 	public static void main(String[] args) throws IOException
 	{
