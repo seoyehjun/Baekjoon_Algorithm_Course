e3aae62d7f8614ce1664665c22e1078bb35223bf
서예준 <skgy322@gmail.com>
2023-09-24 03:31:14 +0900

    Silver achieved

--- Baekjoon_GeneralMath/src/baek_1193/FindNum.java
+++ Baekjoon_GeneralMath/src/baek_1193/FindNum.java
@@ -8,42 +8,40 @@ public class FindNum
 {
 	public static void main(String[] args) throws IOException
 	{
-		int[][] list = new int[1000][1000];
-		
-		int now_y=0, now_x=0;
+		int now_y=1, now_x=1;
 		BufferedReader br = 
 				new BufferedReader(new InputStreamReader(System.in));
-		
 		int total_num = Integer.parseInt(br.readLine());
-		int count=0;
+		int count=1;
 		char dimension = 'x';
 		int dir_y=1, dir_x=-1;
 		
-		for(int i=0;i<total_num;i++)
+		for(int i=1;i<total_num;)
 		{
-			for(int j=0;j<count;j++)
-			{
-				now_y+=dir_y;
-				now_x+=dir_x;
-				i++;
-				System.out.printf("[y]: %d  [x]: %d \n", now_y,now_x);
-			}
-			
 			if(dimension=='y')
 			{
 				now_y+=1;
 				dimension = 'x';
 				i++;
-				System.out.printf("[y]: %d  [x]: %d \n", now_y,now_x);
+				//System.out.printf("dimen[y]: %d  [x]: %d count: %d\n", now_y,now_x,count);
 			}
 			else//dimension=='x'
 			{
 				now_x+=1;
 				dimension = 'y';
 				i++;
-				System.out.printf("[y]: %d  [x]: %d \n", now_y,now_x);
+				//System.out.printf("dimen[y]: %d  [x]: %d count: %d\n", now_y,now_x,count);
 			}
 			
+			if(i>=total_num)break;
+			for(int j=1;j<=count;j++)
+			{
+				now_y+=dir_y;
+				now_x+=dir_x;
+				i++;
+				if(i>=total_num)break;
+				//System.out.printf("[y]: %d  [x]: %d count: %d\n", now_y,now_x,count);
+			}
 			//대각선 위쪽방향 or 아랫쪽방향
 			//count가 늘어남으로 대각선의 길이도 점점 늘어남
 			dir_y*=-1;
