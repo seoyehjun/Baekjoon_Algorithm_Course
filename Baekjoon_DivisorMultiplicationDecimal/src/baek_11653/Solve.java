f9c5be120e339583189270c5c6287776fc9c9953
holy38 <63962696+seoyehjun@users.noreply.github.com>
2023-09-27 15:13:58 +0900

    Divisor_Finished

--- Baekjoon_DivisorMultiplicationDecimal/src/baek_11653/Solve.java
+++ Baekjoon_DivisorMultiplicationDecimal/src/baek_11653/Solve.java
@@ -12,15 +12,26 @@ public class Solve
 		
 		int target = Integer.parseInt(br.readLine());
 		
-		while(target)
-		for(int i=2;i<=target;i++)
+		int count=1;
+		while(count>0)
 		{
-			if(target%i==0)
+			count=0;
+			
+			for(int i=2;i<target;i++)
 			{
-				target /=i;
-				System.out.println(i);
+				if(target%i==0)//약수가 있다면(약수중 제일 작은 약수)
+				{
+					count++;//소수가 아님을 판별
+					target /=i;
+					System.out.println(i);
+					i--;// 2 2 2 처럼 2로 3번연속 나눌 수 있으니까
+				}
 			}
-			i--;
+		}
+		
+		if(target !=1)
+		{
+			System.out.println(target);//1은 출력하지 않는다. 
 		}
 		
 	}
