31359d28247a04d99276469238373ab773e374e7
holy38 <63962696+seoyehjun@users.noreply.github.com>
2023-10-04 10:18:37 +0900

    time 3

---
+++ Baekjoon_Time/src/baek_24263/Time.java
@@ -0,0 +1,16 @@
+package baek_24263;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Time 
+{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(br.readLine());
+		System.out.println("1");
+	}
+}
---
+++ Baekjoon_Time/src/baek_24264/Time.java
@@ -0,0 +1,16 @@
+package baek_24264;
+
+import java.util.Scanner;
+
+public class Time 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		long input = sc.nextLong();
+		System.out.println((long)Math.pow(input, 2));
+		System.out.println(2);
+		
+	}
+}
