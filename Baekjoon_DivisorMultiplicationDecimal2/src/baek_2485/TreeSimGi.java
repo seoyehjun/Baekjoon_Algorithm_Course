c2c8646671aeea0c90d5c1cb25a182ed7f5b922b
서예준 <skgy322@gmail.com>
2023-10-27 02:16:00 +0900

    tree finished

--- Baekjoon_DivisorMultiplicationDecimal2/src/baek_2485/TreeSimGi.java
+++ Baekjoon_DivisorMultiplicationDecimal2/src/baek_2485/TreeSimGi.java
@@ -3,38 +3,52 @@ package baek_2485;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class TreeSimGi
 {
+	public static int euc(int a, int b)
+	{
+		if(b==0)
+		{
+			return a;
+		}
+		else
+		{
+			return euc(b,a%b);
+		}
+	}
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
-		int gap =1000000000;
 		int[] list = new int[N];
 		
-		list[0] = Integer.parseInt(br.readLine());
-		for(int i=1;i<N;i++)
+		for(int i=0;i<N;i++)
 		{
 			list[i] = Integer.parseInt(br.readLine());
-			if(list[i]-list[i-1]<gap)gap = list[i]-list[i-1];
 		}
 		
-		int count =0, nowval=list[0];
+		int round_euc=list[1]-list[0];
 		
-		while()
+		for(int i=2;i<N;i++)
 		{
-			int i=0;
-			if(list[i] == nowval + gap)
-			{
-				i++;
-				nowval = list[i];
-			}
-			else 
-			{
-				nowval +=gap;				
-			}
+			//System.out.println("list[i-1]: "+list[i-1]);
+			//System.out.println("list[i]: " +list[i]);
+			
+			round_euc = euc(round_euc,list[i]-list[i-1]);
 		}
 		
+		int count =0;
+		
+		//System.out.println("round_euc: "+round_euc);
+		for(int i=1;i<N;i++)
+		{
+			//System.out.println("list[i]-list[i-1]: "+(list[i]-list[i-1]));
+			count += (list[i]-list[i-1])/round_euc-1;
+		}
+		
+		
+		System.out.println(count);
 	}
 }
